f5609d1759d25b39615e358499a09c9db71e3025
archimekai <wenkai123111@126.com>
2016-09-11 18:41:34 +0800

    add new files

---
+++ getDemoRoad.m
@@ -0,0 +1,3 @@
+function road = getDemoRoad()
+road =  RoadInstance(2, 3.5, 900, 2, 50/3.6, 0, 30, 1, -1, -1);
+end
---
+++ plotCapByNCrossAndLength.m
@@ -0,0 +1,4 @@
+%% 画出通行能力随交叉路口数量和道路长度的变化情况
+function plotCapByNCrossAndLength()
+
+end
